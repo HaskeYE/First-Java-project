1ef1e9c5242903ec53722d3ce2ca1346db287253
HaskeYE <yegor475@bk.ru>
2019-02-12 22:50:21 +0300

    First Java project

---
+++ src/HashTable.java
@@ -0,0 +1,9 @@
+import java.util.Set;
+
+public class HashTable {
+    Set<Integer> numerals; //цифры, которые будут записаны в хеш-таблицу(ключи)
+    String hashFunction; //хеш-функция
+    public build(HashTable){
+
+    }
+}
