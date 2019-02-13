202630bddeab4c187913208586ec3d39c783acb7
HaskeYE <yegor475@bk.ru>
2019-02-13 12:42:41 +0300

    First Java project

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,24 +1,24 @@
-import sun.invoke.empty.Empty;
 
-import java.lang.reflect.Array;
+
+
 import java.util.Set;
+
 
 public class HashTable {
     static Set<Integer> numerals; //цифры, которые будут записаны в хеш-таблицу(ключи)
     static String hashFunction; //хеш-функция
 
-    public static void build(Array args[]) {
+    public static Set[] build(HashTable args[]) {
         int function = Character.getNumericValue(hashFunction.charAt(1));
-        Array[] hashtable = new Array[function];
+        Set[] hashtable = new Set[function];
         for (int numeral : numerals) {
             int hash = numeral % function;
-            if (hashtable[hash] == null) {
-                hashtable[hash] = new Set(numeral);
-            } else {
-                hashtable[hash] + new Array(numeral);
+                hashtable[hash].add(numeral);
             }
+            return hashtable;
         }
     }
 
-}
+
+
 
---
+++ src/TestTable.java
@@ -0,0 +1,8 @@
+import java.util.Set;
+
+public class TestTable {
+    public static void main(Set[] args[]){
+        HashTable table = new HashTable();
+        table.numerals = new (1, 2, 4, 6, 12, 14, 18);
+    }
+}
