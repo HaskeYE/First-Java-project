10e972501b192df0a80d5d96f6753847bdef9b78
HaskeYE <yegor475@bk.ru>
2019-02-16 19:13:34 +0300

    First Java project

--- src/TestTable.java
+++ src/TestTable.java
@@ -1,8 +1,7 @@
 import java.util.Set;
 
-public class TestTable {
+public class TestTable {/*
     public static void main(Set[] args[]){
         HashTable table = new HashTable();
-        table.numerals = new (1, 2, 4, 6, 12, 14, 18);
-    }
+    }*/
 }
