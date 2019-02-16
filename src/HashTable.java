1d8d5c0558fba59d5b79c1d24530d903cf4f396d
HaskeYE <yegor475@bk.ru>
2019-02-16 18:35:16 +0300

    First Java project

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,23 +1,41 @@
 
 
 
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 
 public class HashTable {
-    static Set<Integer> numerals; //цифры, которые будут записаны в хеш-таблицу(ключи)
-    static String hashFunction; //хеш-функция
+    ArrayList<LinkedList<Integer>> numerals;
+    private int hashDiv; //хеш-функция деления
 
-    public static Set[] build(HashTable args[]) {
+    public HashTable(ArrayList<Integer> list, int hashDiv) {
+        numerals = new ArrayList<>(hashDiv - 1);
+        this.hashDiv = hashDiv;
+        for (Integer i : list) {
+            numerals.get(indexForHash(hash(i))).add(i);
+        }
+    }
+
+    private int hash(int numeral) {
+        return numeral % hashDiv;
+    }
+
+    private int indexForHash(int hash) {
+//Ддя возможно необходимых дальнейших операций
+        return hash;
+    }
+
+    /*public Set[] build(HashTable args[]) {
         int function = Character.getNumericValue(hashFunction.charAt(1));
         Set[] hashtable = new Set[function];
         for (int numeral : numerals) {
             int hash = numeral % function;
-                hashtable[hash].add(numeral);
-            }
-            return hashtable;
+            hashtable[hash].add(numeral);
         }
-    }
+        return hashtable;
+    }*/
+}
 
 
 
